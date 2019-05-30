75ad11386d51339cb32d4554649b911e273a8bab
Ronaldo Leite <rleite1975@gmail.com>
2019-05-29 22:24:13 -0300

    Persistence: Spring Data JPA

---
+++ SpringData/src/main/java/model/Produto.java
@@ -0,0 +1,44 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+    @Id
+    public Integer id;
+    public String nome;
+
+    public Produto() {
+
+    }
+
+    public Produto(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+}
---
+++ SpringData/src/main/java/service/ProdutoService.java
@@ -0,0 +1,36 @@
+package service;
+
+import model.Produto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import repository.ProdutoRepository;
+
+import java.util.Collection;
+import java.util.List;
+
+@Component
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Transactional
+    public void add(Produto produto) {
+        produtoRepository.save(produto);
+    }
+    @Transactional(readOnly=true)
+    public List<Produto> findAll() {
+        return produtoRepository.findAll();
+    }
+    @Transactional
+    public void addAll(Collection<Produto> produtos) {
+        for (Produto produto : produtos) {
+            produtoRepository.save(produto);
+        }
+    }
+    @Transactional(readOnly=true)
+    public List<Produto> findByName(String nome) {
+        return produtoRepository.findByName(nome);
+    }
+}
